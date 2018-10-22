a8b593185cde5f925d0309b40be813f95427e9fa
zhanglinw0822 <zhanglinw0822@hotmail.com>
2018-10-22 21:30:20 +0800

    Update send_mail.py

--- send_mail.py
+++ send_mail.py
@@ -9,19 +9,22 @@ def _format_addr(s):
 
 
 def send_email(msg, to_addr=None):
-    if to_addr is None:
-        to_addr = ['zhagnlinw0822@163.com']
-    from_addr = 'zlw0822@163.com'
-    password = 'wei860822'
-    smtp_server = 'smtp.163.com'
-    msg = MIMEText(msg, 'plain', 'utf-8')
-    msg['From'] = _format_addr('下单服务 <%s>' % from_addr)
-    msg['Subject'] = Header('服务异常', 'utf-8').encode()
-    server = smtplib.SMTP(smtp_server, 25)
-    server.set_debuglevel(1)
-    server.login(from_addr, password)
-    server.sendmail(from_addr, to_addr, msg.as_string())
-    server.quit()
+    try:
+        if to_addr is None:
+            to_addr = ['zhagnlinw0822@163.com']
+        from_addr = 'zlw0822@163.com'
+        password = 'wei860822'
+        smtp_server = 'smtp.163.com'
+        msg = MIMEText(msg, 'plain', 'utf-8')
+        msg['From'] = _format_addr('下单服务 <%s>' % from_addr)
+        msg['Subject'] = Header('服务异常', 'utf-8').encode()
+        server = smtplib.SMTP(smtp_server, 25, timeout=1)
+        server.set_debuglevel(1)
+        server.login(from_addr, password)
+        server.sendmail(from_addr, to_addr, msg.as_string())
+        server.quit()
+    except Exception as e:
+        print("邮件发送失败")
 
 if __name__ == '__main__':
     to_addr = ['zhanglinw0822@163.com']
